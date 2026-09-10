567675acf3754d4d4b87671a8c33edab2ef9b643
NivGreenstein <NivGreenstein@users.noreply.github.com>
2026-09-10 07:02:29 +0000

    Deploy preview for PR 16 🛫

---
+++ pr-preview/pr-16/assets/js/6d204b8a.05271e4b.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkshigola_docs||=[]).push([[784],{4375(s){s.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"tutorials"}')}}]);
---
+++ pr-preview/pr-16/assets/js/a7456010.b6a3bd2a.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkshigola_docs||=[]).push([[235],{8552(s){s.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
